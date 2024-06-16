e9ae9cdb14d8ab586bacb7aac4e75bf1ea42b04b
dickyhikam <dicky.steven79@yahoo.com>
2024-06-16 11:09:27 +0700

    Landing Page - Tutorial

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,7 +11,7 @@
                 <h1 data-aos="fade-up">Nikmati Internet Cepat</h1>
                 <p data-aos="fade-up" data-aos-delay="100">Unlimited & Murah</p>
                 <br>
-                <h2 data-aos="fade-up" class="mb-3">Mulai Dari Rp. 140.000/Bulan*</h2>
+                <h2 data-aos="fade-up" class="mb-3">Mulai Dari Rp. {{number_format($data_postingan_harga, 0, ',', '.')}}/Bulan*</h2>
 
                 <p data-aos="fade-up" data-aos-delay="100" style="font-size: 14px;">*Syarat dan Ketentuan Berlaku</p>
                 <div class=" d-flex flex-column flex-md-row" data-aos="fade-up" data-aos-delay="200">
@@ -107,71 +107,24 @@
     <div class="container">
 
         <div class="row">
-
+            @foreach($data_postingan as $item)
             <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
-
                 <div class="faq-container">
-
-                    <div class="faq-item faq-active">
-                        <h3>Non consectetur a erat nam at lectus urna duis?</h3>
-                        <div class="faq-content">
-                            <p>Feugiat pretium nibh ipsum consequat. Tempus iaculis urna id volutpat lacus laoreet non curabitur gravida. Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus non.</p>
-                        </div>
-                        <i class="faq-toggle bi bi-chevron-right"></i>
-                    </div><!-- End Faq item-->
-
                     <div class="faq-item">
-                        <h3>Feugiat scelerisque varius morbi enim nunc faucibus a pellentesque?</h3>
+                        <h3>{{ $item->judul }}</h3>
                         <div class="faq-content">
-                            <p>Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit laoreet id donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Est pellentesque elit ullamcorper dignissim. Mauris ultrices eros in cursus turpis massa tincidunt dui.</p>
+                            <p>
+                                {{ $item->keterangan }}
+                                <br>
+                                <iframe width="640" height="360" src="{{ $item->link_video }}" frameborder="0" allowfullscreen></iframe>
+                            </p>
+
                         </div>
                         <i class="faq-toggle bi bi-chevron-right"></i>
                     </div><!-- End Faq item-->
-
-                    <div class="faq-item">
-                        <h3>Dolor sit amet consectetur adipiscing elit pellentesque?</h3>
-                        <div class="faq-content">
-                            <p>Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Faucibus pulvinar elementum integer enim. Sem nulla pharetra diam sit amet nisl suscipit. Rutrum tellus pellentesque eu tincidunt. Lectus urna duis convallis convallis tellus. Urna molestie at elementum eu facilisis sed odio morbi quis</p>
-                        </div>
-                        <i class="faq-toggle bi bi-chevron-right"></i>
-                    </div><!-- End Faq item-->
-
                 </div>
-
             </div><!-- End Faq Column-->
-
-            <div class="col-lg-6" data-aos="fade-up" data-aos-delay="200">
-
-                <div class="faq-container">
-
-                    <div class="faq-item">
-                        <h3>Ac odio tempor orci dapibus. Aliquam eleifend mi in nulla?</h3>
-                        <div class="faq-content">
-                            <p>Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit laoreet id donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Est pellentesque elit ullamcorper dignissim. Mauris ultrices eros in cursus turpis massa tincidunt dui.</p>
-                        </div>
-                        <i class="faq-toggle bi bi-chevron-right"></i>
-                    </div><!-- End Faq item-->
-
-                    <div class="faq-item">
-                        <h3>Tempus quam pellentesque nec nam aliquam sem et tortor consequat?</h3>
-                        <div class="faq-content">
-                            <p>Molestie a iaculis at erat pellentesque adipiscing commodo. Dignissim suspendisse in est ante in. Nunc vel risus commodo viverra maecenas accumsan. Sit amet nisl suscipit adipiscing bibendum est. Purus gravida quis blandit turpis cursus in</p>
-                        </div>
-                        <i class="faq-toggle bi bi-chevron-right"></i>
-                    </div><!-- End Faq item-->
-
-                    <div class="faq-item">
-                        <h3>Perspiciatis quod quo quos nulla quo illum ullam?</h3>
-                        <div class="faq-content">
-                            <p>Enim ea facilis quaerat voluptas quidem et dolorem. Quis et consequatur non sed in suscipit sequi. Distinctio ipsam dolore et.</p>
-                        </div>
-                        <i class="faq-toggle bi bi-chevron-right"></i>
-                    </div><!-- End Faq item-->
-
-                </div>
-
-            </div><!-- End Faq Column-->
-
+            @endforeach
         </div>
 
     </div>
